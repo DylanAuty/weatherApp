a5e8f73d8bf5ce259010bf0be4503e215042c093
Dylan Auty <dylan.auty@gmail.com>
2017-03-19 19:42:50 +0000

    Removing and re-adding stuff to respect the gitignore

--- weatherApp/views.py
+++ weatherApp/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+import requests
+import json
+
 def index(request):
-    return HttpResponse("Hello, world")
 
+    context = {
 
+            }
+    return render(request, 'weatherApp/index.html', context)
+
+def getCurrentWeather(request):
+    API_response = requests.get('http://api.wunderground.com/api/b405479b9102f47f/forecast/q/CA/San_Francisco.json')
+    return HttpResponse(API_response)
 
 
 
--- weatherProj/urls.py
+++ weatherProj/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 
 
 urlpatterns = [
-    url(r'^$', include('weatherApp.urls')),
+    url(r'^', include('weatherApp.urls')),
     url(r'^admin/', admin.site.urls),
 ]
