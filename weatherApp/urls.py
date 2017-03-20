64355ec163dd6cbb18a40be40bef4d23f5618170
Dylan Auty <dylan.auty@gmail.com>
2017-03-20 22:05:08 +0000

    Added getExampleForecastJSON to use while debugging, to avoid running over API key limits

--- weatherApp/urls.py
+++ weatherApp/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name="index"),
     url(r'^getCurrentWeather/$', views.getCurrentWeather, name="getCurrentWeather"),
+    url(r'^getExampleForecastJSON/$', views.getExampleForecastJSON, name="getExampleForecastJSON"),
 ]
 
 
--- weatherApp/views.py
+++ weatherApp/views.py
@@ -1,19 +1,36 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from django.conf import settings
 import requests
 import json
 
 def index(request):
-
+    # Retrieve most recent entry for London from DB, stick into context and return it.
+    # If one doesn't exist, then fire the querying method.
+    
     context = {
 
             }
     return render(request, 'weatherApp/index.html', context)
 
 def getCurrentWeather(request):
-    API_response = requests.get('http://api.wunderground.com/api/b405479b9102f47f/forecast/q/CA/San_Francisco.json')
+    # Dummy placeholder text
+    locationString = "/q/zmw:00000.1.03772"
+    #requestURL = "http://api.wunderground.com/api/" + settings.WUNDERGROUND_API_KEY + "/forecast" + locationString + ".json"
+    requestURL = "file:///home/da-sol-vb/Documents/HealthUnlocked/forecastEx.json"
+
+    API_response = requests.get(requestURL)
+    #print(type(API_response.json()))
+    #API_response_JSON = json.loads(API_response)
+    #print(API_response_JSON.dumps)
+
     return HttpResponse(API_response)
 
 
-
+def getExampleForecastJSON(request):
+    """
+    Retrieves an example forecast for London from local.
+    Used for debugging, to avoid exceeding API limits.
+    """
+    return render(request, 'weatherApp/forecastEx.json')
