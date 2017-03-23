7678666340854f631823023ec1592d03853bdaf4
Dylan Auty <dylan.auty@gmail.com>
2017-03-23 21:14:49 +0000

    About page URL

--- weatherApp/urls.py
+++ weatherApp/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name="index"),
+    url(r'^about/$', views.about, name="about"),
     url(r'^getCurrentWeather/$', views.getCurrentWeather, name="getCurrentWeather"),
     url(r'^getExampleForecastJSON/$', views.getExampleForecastJSON, name="getExampleForecastJSON"),
 ]
