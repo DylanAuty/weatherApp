3388fa523b3e0aa63fe4bc03bc9d9afba192ed01
Dylan Auty <dylan.auty@gmail.com>
2017-03-29 02:02:22 +0100

    Added offline locationEx thing

--- weatherApp/urls.py
+++ weatherApp/urls.py
@@ -1,13 +1,16 @@
 from django.conf.urls import url
 
 from . import views
+from weatherApp.utils import utils
 
 urlpatterns = [
     url(r'^$', views.index, name="index"),
     url(r'^about/$', views.about, name="about"),
  #   url(r'^getCurrentWeather/$', views.getCurrentWeather, name="getCurrentWeather"),
     url(r'^getExampleForecastJSON/$', views.getExampleForecastJSON, name="getExampleForecastJSON"),
-]
+    url(r'^getExampleLocationJSON/$', views.getExampleForecastJSON, name="getExampleForecastJSON"),
+    url(r'^getAutocompleteResults/$', utils.getAutocompleteResults, name="getAutocompleteResults"),
+    ]
 
 
 
